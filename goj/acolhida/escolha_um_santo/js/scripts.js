69c25d1b5daf751b2f1bca4124f4c5788d7d2c23
Robson <robalbuquerque98@gmail.com>
2024-06-03 16:47:35 -0300

    feat: adiciona funcionalidades JavaScript básicas

---
+++ goj/acolhida/escolha_um_santo/js/scripts.js
@@ -0,0 +1,110 @@
+const mensagens = {
+    "São Francisco de Assis": [
+        "Para pregar a Paz primeiro você deve ter a Paz dentro de você.",
+        "Comece fazendo o que é necessário, depois o que é possível, e de repente você estará fazendo o impossível.",
+        "O que temer? Nada. A quem temer? Ninguém. Por quê? Porque aqueles que se unem a Deus obtêm três grandes privilégios: onipotência, sem poder, embriaguez, sem vinho, e vida sem morte.",
+        "Onde há amor e sabedoria, não tem temor e nem ignorância.",
+        "Que o senhor volte os olhos para ti e te dê tua paz. Que o senhor te abençoe.",
+        "Apenas um raio de sol é suficiente para afastar várias sombras.",
+        "Ninguém é suficientemente perfeito, que não possa aprender com o outro e, ninguém é totalmente estruído de valores que não possa ensinar algo ao seu irmão.",
+        "Senhor, dai-me força para mudar o que pode ser mudado... Resignação para aceitar o que não pode ser mudado... E sabedoria para distinguir uma coisa da outra.",
+        "A cortesia é irmã da caridade, que apaga o ódio e fomenta o amor.",
+        "Não vos esforceis pelas honras do mundo, mas honrai o SENHOR."
+    ],
+    "Santa Teresinha do Menino Jesus": [
+        "Faça o bem sem olhar a quem.",
+        "O bom Deus me dá a coragem na proporção dos meus sofrimentos.",
+        "O que ofende Jesus é a falta de confiança.",
+        "O prisoneiro do amor! Ah! Para a Hóstia minha alma aspira. Eu a amo e não quero mais nada. É o Deus escolhido que me fascina.",
+        "Jesus não exige grandes ações de nós, mas simplesmente entrega e gratidão.",
+        "Nada, a não ser o amor de Jesus, poderia ter me feito enfrentar essas dificuldades e outras que se seguiram, pois tive que comprar minha felicidade por meio de pesadas provações.",
+        "Sei agora que a verdadeira caridade consiste em suportar todos os defeitos do próximo - não ficar surpreso com sua fraqueza, mas edificado em suas menores virtudes."
+    ],
+    "São João Paulo II": [
+        "A violência destrói o que ela pretende defender: a dignidade da vida, a liberdade do ser humano.",
+        "A loucura de Deus é muito mais bonita que a sabedoria do homem.",
+        "A paz exige quatro condições essenciais: verdade, justiça, amor e liberdade.",
+        "A pior prisão é um coração fechado.",
+        "Aprendei a ouvir no silêncio a voz de Deus, que fala no mais fundo de cada um de nós.",
+        "O ser humano sem Deus não pode compreender  a si mesmo; como, também, não poderá realizar-se sem Deus.",
+        "O amor me explicou tudo..."
+    ],
+    "Carlo Acutis": [
+        "A única coisa que nós temos que pedir a Deus na oração é a vontade de ser santos.",
+        "O balão necessita de descarregar os seus pesos para poder elevar-se. Assim também a alma, para chegar ao céu, precisa de remover até às pequenas cargas que são os pecados veniais.",
+        "Ser cristão, para mim, significa observar o mundo e levar minha alegria e a minha força aos demais.",
+        "Peça ao seu Anjo da Guarda para ajudá-lo continuamente, de modo que ele se torne Seu melhor amigo.",
+        "Todos os dias vivo a Eucaristia como um diálogo constante com Jesus, como uma autêntica esperança. A Eucaristia é a minha autoestrada para o céu.",
+        "Os amigos são uma das nossas maiores riquezas, devem ser cultivados, com muito cuidado e ternura.",
+        "A tristeza é o olhar voltado para si; a felicidade é o olhar voltado para Jesus.",
+        "Estar sempre junto com Jesus, este é o meu plano de vida."
+    ],
+    "Santa Madre Tereza de Calcutá": [
+        "Não devemos permitir que alguém saia da nossa presença sem se sentir melhor e mais feliz.",
+        "É fácil amar os que estão longe. Mas nem sempre é fácil amar os que vivem ao nosso lado.",
+        "Por vezes sentimos que aquilo que fazemos não é senão uma gota de água no mar. Mas o mar seria menor se lhe faltasse uma gota.",
+        "Quem julga as pessoas não tem tempo para amá-las.",
+        "Todas as nossas palavras serão inúteis se não brotarem do fundo do coração. As palavras que não dão luz aumentam a escuridão.",
+        "A falta de amor é a maior de todas as pobrezas.",
+        "O importante não é o que se dá, mas o amor com que se dá.",
+        "Temos de ir à procura das pessoas, porque podem ter fome de pão ou de amizade.",
+        "Senhor, ajuda-me a derramar Tua fragância aonde quer que eu vá."
+    ],
+    "Santo Agostinho": [
+        "A medida do amor é amar sem medida.",
+        "O orgulho é a fonte de todas as fraquezas, porque é a fonte de todos os vícios.",
+        "Não há lugar para a sabedoria onde não há paciência.",
+        "Não basta fazer coisas boas. É preciso fazê-las bem.",
+        "Não é tanto o que fazemos, mas o motivo pelo qual fazemos que determina a bondade ou a malícia.",
+        "Sê humilde para evitar o orgulho, mas voa alto para alcançar a sabedoria.",
+        "Enquanto houver vontade de lutar haverá esperança de vencer.",
+        "Ter fé é assinar uma folha em branco e deixar que Deus nela escreva o que quiser."
+    ],
+    "São Pedro": [
+        "Tende, antes de tudo, ardente Amor uns para com os outros, porque o Amor cobre uma multidão de pecados.",
+        "O mais importante de tudo é continuarem a mostrar um profundo amor uns pelos outros, pois o amor compensa muitas das faltas de vocês. E todos vocês sirvam uns aos outros com um espírito humilde, pois Deus concede bênçãos especiais àqueles que são humildes, mas se opõe àqueles que são orgulhosos.",
+        "Senhor, Tu sabes todas as coisas; Tu sabes que Te amo."
+    ],
+    "Senhora de Fátima": [
+        "Nossa Senhora de Fátima, agradeço o seu amparo e a sua proteção, rogando-lhe que jamais abandone o coração dos aflitos. Que o seu dia seja celebrado com fé e esperança de que tudo nessa vida pode ser transformado e que o bem sempre prevaleça sobre o mal. Amém!",
+        "Nossa Senhora de Fátima é minha mãe amada que me leva para mais perto do Senhor por meio do Santo Rosário."
+    ],
+    "São Bejamim": [
+        "Para mim o viver é Cristo, e o morrer é ganho",
+        "Perdão é, na pior das hipóteses, assumir erros que não são seus. Parece ser estupidez, mas na verdade é ser como Cristo."
+    ],
+    "Santo André": [
+        "Viver é não fugir à luta."
+    ],
+    "Nossa Senhora da Conceição": [
+        "Se a Senhora estiver comigo, não terei nada a temer, minha Mãe, porque, com seu amor, sei que vou superar todas as dificuldades.",
+        "Que a Senhora esteja sempre ao meu lado e ao lado dos necessitados, Nossa Senhora da Imaculada Conceição.",
+        "Que seus braços me abracem sempre que eu precisar e que minha oração chegue a Ti, Nossa Senhora da Imaculada Conceição.",
+        "Mãe, cubra-me com seu manto e faça-me ter coragem e serenidade para enfrentar os desafios da vida.",
+        "Mãe, humildemente te peço, retire de mim o véu da tristeza e permita-me viver na luz do seu amor.",
+        "Querida Mãe, Nossa Senhora da Imaculada Conceição, ilumine meus passos e faça-me digno de sua amorosa proteção."
+    ],
+    "São Bento": [
+        "Crux Sacra Sit Mihi Lux. Non Draco Sit Mihi Dux. Vade Retro Satana! Nunquam Suade Mihi Vana. Sunt Mala Quae Libas. Ipse Venena Bibas. | A Cruz sagrada seja minha Luz. Não seja o Dragão meu guia. Retira-te Satanás. Nunca me aconselhes coisas vãs. É mal o que tu me ofereces. Bebe tu mesmo do teu veneno.",
+        "⁠Ora Et Labora (ora e trabalha).",
+        "Não faças ⁠injustiça, mas suporte pacientemente as que lhe são feitas.Vigiea toda hora os atos de sua vida. Sabei como certo que Deus o vê em todo lugar.",
+        "Antes de tudo, quando quiseres realizar algo de bom, pede a Deus com oração muito insistente que seja plenamente realizado por Ele."
+    ]
+    // Adicione mais mensagens para os santos aqui
+};
+
+const santoDivs = document.querySelectorAll('.santo');
+
+santoDivs.forEach((santoDiv) => {
+    santoDiv.addEventListener('click', () => {
+        const santo = santoDiv.getAttribute('data-santo');
+        const mensagensSanto = mensagens[santo];
+        const mensagem = mensagensSanto[Math.floor(Math.random() * mensagensSanto.length)];
+        exibirMensagem(mensagem);
+    });
+});
+
+function exibirMensagem(mensagem) {
+    const mensagemTexto = document.getElementById('messagem-texto');
+    mensagemTexto.textContent = mensagem;
+}
